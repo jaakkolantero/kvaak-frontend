42935252ff96ad5e1326f18ea5698e6feec5f373
UFo_x <tero.jaakkola1@gmail.com>
2018-01-17 22:10:26 +0200

    Add basic routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
-import {
-        Hero,
-        HeroBody,
-        HeroHead,
-        Container,
-        Title,
-        SubTitle
-        } from 'reactbulma'
+import Navigation from './components/Navigation';
+import {Hero, Container, Title, SubTitle} from 'reactbulma'
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
+        <Navigation />
         <Hero primary>
           <Hero.Body>
             <Container>
---
+++ src/components/Navigation.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+
+class Navigation extends Component {
+  constructor(props) {
+    super(props);
+    this.toggleActive= this.toggleActive.bind(this);
+    this.state = {
+        isActive: false,
+    };
+  }
+
+  toggleActive() {
+    this.setState({ isActive: !this.state.isActive });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <nav className="navbar">
+          <div className="container">
+            <div className="navbar-brand">
+              <NavLink to="/" className="navbar-item">
+                <h1 className="title">KvaaK!</h1>
+              </ NavLink>
+              <span className={this.state.isActive ? 'navbar-burger burger  is-active' : 'navbar-burger burger'}  onClick={this.toggleActive}>
+                <span></span>
+                <span></span>
+                <span></span>
+              </span>
+            </div>
+            <div id="navbarMenu" className={this.state.isActive ? 'navbar-menu is-active' : 'navbar-menu'}>
+              <div className="navbar-end">
+                <NavLink to="/Sightings" activeClassName="is-active" className="navbar-item">Sightings</NavLink>
+                <NavLink to="/Species" activeClassName="is-active" className="navbar-item">Species</NavLink>
+                <NavLink to="/AddSighting" activeClassName="is-active" className="navbar-item">Add sighting</NavLink>
+                <NavLink to="/Settings" activeClassName="is-active" className="navbar-item">
+                  <a className="navbar-item">
+                    <span className="icon is-large">
+                        <i className="fa fa-2x fa-cog"></i>
+                    </span>
+                  </a>
+                </ NavLink>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </ React.Fragment>
+    );
+  }
+}
+
+export default Navigation;
