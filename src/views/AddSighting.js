6627ef6af56b9b141a861e8e15f9f40b880456ec
UFo_x <tero.jaakkola1@gmail.com>
2018-01-26 21:38:00 +0200

    First draft of AddSighting form.

--- src/views/AddSighting.js
+++ src/views/AddSighting.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Header from 'components/Header';
 import Calendar from 'components/Calendar/Calendar';
-import {Container} from 'bloomer';
+import {Container, Columns, Column, Field, Label, Control, Input, Select, TextArea, Button} from 'bloomer';
 
 class AddSighting extends Component {
   constructor (props) {
@@ -22,8 +22,39 @@ class AddSighting extends Component {
       <React.Fragment>
         <Header title="Add Sighting" subtitle="Sharing is caring." />
         <Container style={{ marginTop: 30, marginBottom: 30 }}>
-          <p>With <strong>KvaaK!</strong> you can checkout what birds have been spotted by your twitcher friends and report your own sightings!</p>
-          <Calendar changeDate={this.changeDate} language="en"/>
+          <Columns isCentered>
+            <Column isSize='1/2' style={{ marginLeft: 15 }}>
+            <Field>
+              <Label>Species:</Label>
+              <Control>
+                <Select>
+                  <option>Mallard</option>
+                  <option>Mockingbird</option>
+                </Select>
+              </Control>
+            </Field>
+            <Field>
+              <Label>Count</Label>
+              <Control>
+                <Input  type="number" placeholder='Count' />
+              </Control>
+            </Field>
+            <Field>
+              <Label>Description</Label>
+              <Control>
+                <TextArea placeholder={'Description..'} />
+              </Control>
+            </Field>
+            <Field isGrouped>
+              <Control>
+                <Button isColor='primary'>Submit</Button>
+              </Control>
+            </Field>
+            </Column>
+            <Column isSize='1/2' style={{ marginLeft: 15 }}>
+              <Calendar changeDate={this.changeDate} language="en"/>
+            </Column>
+          </Columns>
         </Container>
       </ React.Fragment>
     );
