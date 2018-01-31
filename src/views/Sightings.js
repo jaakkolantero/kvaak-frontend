93cec006fa0b62f5ef0a3534a0ac8d792166a365
UFo_x <tero.jaakkola1@gmail.com>
2018-01-31 21:23:56 +0200

    remove dummyData

--- src/views/Sightings.js
+++ src/views/Sightings.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import dummyData from './sightings.json';
 import Header from 'components/Header';
 import SightingItem from 'components/SightingItem';
 import {Container} from 'bloomer';
